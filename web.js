90fb88e34f40680644dffce2e7cdbe47f1e52b52
Christopher Toledo <uberscientist@gmail.com>
2013-07-04 14:14:28 -0700

    Added index.html, gitignore and altered web.js

--- web.js
+++ web.js
@@ -1,12 +1,15 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var file = fs.readFileSync("./index.html").toString("utf8");
+  response.send(file);
 });
 
 var port = process.env.PORT || 5000;
+
 app.listen(port, function() {
   console.log("Listening on " + port);
 });

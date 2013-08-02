eaf1fd24378ef5ecac20477ae6c353b904b8c854
Christopher Toledo <uberscientist@gmail.com>
2013-08-02 10:33:58 -0700

    public dir, bootstrap 3

--- web.js
+++ web.js
@@ -4,11 +4,6 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
-  var file = fs.readFileSync("./index.html").toString("utf8");
-  response.send(file);
-});
-
 var port = process.env.PORT || 8080;
 
 app.listen(port, function() {

c060194ef0af687a033913090bbb21a0d84f06f8
Christopher Toledo <uberscientist@gmail.com>
2013-08-03 10:50:18 +0400

    Donation functionality beginnings

---
+++ public/js/donate.js
@@ -0,0 +1,23 @@
+$.ready = function() {
+
+  var displayAddress = function(coin_type) {
+    var name = $('#donater-name').val() || 'Anonymous';
+
+    $.get('/donate-api/address/' + coin_type + '/' + name, function(data) {
+
+      if(!data.error) {
+        $('#address').html(data.address);
+        $('#qr-code').attr('src', data.qr).fadeIn();
+        $('#address-container').fadeIn();
+        $('.hide-after-donate').fadeOut();
+        $('.modal-title').html('Thank you ' + data.name + '!');
+      }
+
+    });
+  }
+
+  $('.coin-select-btn').click(function () {
+    var coin_type = $(this).attr('id');
+    displayAddress(coin_type);
+  });
+};

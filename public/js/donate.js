6efb4efc7ccf713891a4a4ff98e76a90fc606dbf
Christopher Toledo <uberscientist@gmail.com>
2013-08-07 00:07:31 +0400

    RC, displays total backers, list, etc

--- public/js/donate.js
+++ public/js/donate.js
@@ -1,3 +1,13 @@
+// Yoinked out of sugar.js
+var escapeHTML = function(string) {
+  return string.replace(/&/g, '&amp;' )
+              .replace(/</g, '&lt;' )
+              .replace(/>/g, '&gt;' )
+              .replace(/"/g, '&quot;')
+              .replace(/'/g, '&apos;')
+              .replace(/\//g, '&#x2f;');
+}
+
 $.ready = function() {
 
   var displayAddress = function(coin_type) {
@@ -27,15 +37,40 @@ $.ready = function() {
   }
 
   var displayDonations = function() {
+    $.get('/donate-api/donations', function(donations) {
+      var btc = donations[0];
+      var ltc = donations[1];
 
+      var ltc_list = "<ul>"
+      var btc_list = "<ul>"
+
+      for(var i = 0; i < btc.length; i+=2) {
+        var name = escapeHTML(btc[i]);
+        var amount = btc[i+1];
+        btc_list += "<li>" + name + " : " + "&#3647;" + amount + "</li>";
+      }
+      btc_list += "</ul>";
+
+      for(var i = 0; i < ltc.length; i+=2) {
+        var name = escapeHTML(ltc[i]);
+        var amount = ltc[i+1];
+        ltc_list += "<li>" + name + " : " + "&#3647;" + amount + "</li>";
+      }
+      ltc_list += "</ul>";
+
+      $('#btc-donators').html(btc_list);
+      $('#ltc-donators').html(ltc_list);
+    });
   }
 
   var getTotals = function() {
     $.get('/donate-api/totals', function(totals) {
-      console.log(totals);
-      $('#total-btc').html(totals[0]);
-      $('#total-ltc').html(totals[1]);
-    }
+      $('#total-btc').html(totals[0] || '0.0');
+      $('#total-ltc').html(totals[1] || '0.0');
+
+      var donator_count = totals[2] + totals[3];
+      $('#backers-count').html(donator_count);
+    });
   }
 
   // Donation request
@@ -46,4 +81,7 @@ $.ready = function() {
 
   // Display the backer ranks when clicked
   $('#backers-link').click(displayDonations);
+
+  // Display totals for btc/ltc when page loads
+  getTotals();
 };
